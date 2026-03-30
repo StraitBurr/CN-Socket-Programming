810c3bafcd23ab2bfbaed56e219516ed4c3c32d5
Bhooshan <bhooshandeshpande2006@gmail.com>
2026-03-30 14:21:56 +0530

    Added all files

---
+++ client.py
@@ -0,0 +1,32 @@
+from socket import *
+
+servername = "localhost"
+serverport = 12000
+clientSocket = socket(AF_INET,SOCK_STREAM)
+clientSocket.connect((servername,serverport))
+username = input ("Enter username: ")
+while True:
+    print("Distributed Leaderboard System:")
+    print("1. Send score")
+    print("2. Get leaderboard")
+    print("3. Exit")
+
+    sentence = input("Input option(1/2/3): ")
+    if(sentence == "1"):
+        score = input("Enter score: ")
+        sentence = "SEND " + username + " " + score
+    
+    elif(sentence == "2"):
+        sentence = "GET"
+    
+    elif(sentence == "3"):
+        sentence = "EXIT"
+        clientSocket.send(sentence.encode())
+        modifiedSentence = clientSocket.recv(1024).decode()
+        print("From Server: \n" + modifiedSentence)
+        break 
+    
+    clientSocket.send(sentence.encode())
+    modifiedSentence = clientSocket.recv(1024).decode()
+    print("From Server: " + modifiedSentence)
+clientSocket.close()
---
+++ server.py
@@ -0,0 +1,65 @@
+from socket import *
+import threading 
+
+def logic(connectionSocket):
+    print("This will handle the logic of the thread")
+    while True:
+        sentence = connectionSocket.recv(1024).decode()
+        if not sentence:
+            break
+
+        arr = sentence.split()
+        if(arr[0] == "SEND"):
+            if(len(arr) != 3):
+                print("Invalid command")
+                connectionSocket.send("Invalid command".encode())
+                continue
+
+            #update dictionary with {arr[1]: arr[2]}
+            username = arr[1]
+            score = int(arr[2])+leaderboard[username] if username in leaderboard else int(arr[2])
+
+            with lock:
+                leaderboard[username] = score
+
+            print("Updated the leaderboard successfully")
+            connectionSocket.send("Updated the leaderboard successfully".encode())
+
+        elif(arr[0] == "GET"):
+            #return the dictionary or something 
+            print("Return the leaderboard successfully")
+
+            with lock:
+                leaderboard_sorted = dict(sorted(leaderboard.items(), key = lambda item: item[1], reverse=True))
+            
+            result = ""
+            for name, score in leaderboard_sorted.items():
+                   result += name + ": " + str(score) + "\n"
+            print(result.encode())
+            connectionSocket.send(result.encode())
+
+        elif(arr[0] == "EXIT"):
+            print("Closing the connection")
+            connectionSocket.send("Closing the connection".encode())
+            break
+    
+    connectionSocket.close()
+
+
+servername = "localhost"
+serverport = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((servername,serverport))
+serverSocket.listen(1)
+print("The server is ready to receive")
+
+leaderboard = {}
+lock = threading.Lock()
+
+
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    server_thread = threading.Thread(target=logic,args=(connectionSocket,))
+    server_thread.start()
+    
